89fc08967ec82124a40b53219e12b0ec62c6d736
josenildomoments <jnmultimarcastop@gmail.com>
2026-04-30 17:52:08 -0300

    Update page.js

--- app/login/dashboard/page.js
+++ app/login/dashboard/page.js
@@ -1,22 +1,12 @@
-export default function Dashboard() {
-  return (
-    <main style={{ padding: 20, textAlign: "center" }}>
-      <h1>Área interna</h1>
-
-      <p>Bem-vindo ao seu espaço de apoio TEA</p>
-
-      <a href="https://wa.me/5581988057613?text=Preciso de ajuda com TEA">
-        <button style={{
-          padding: 15,
-          marginTop: 20,
-          background: "#25D366",
-          color: "#fff",
-          border: "none",
-          borderRadius: 8
-        }}>
-          Falar no WhatsApp
-        </button>
-      </a>
-    </main>
-  );
-}
+<a href="/dashboard">
+  <button style={{
+    padding: 12,
+    width: "100%",
+    maxWidth: 300,
+    background: "#000",
+    color: "#fff",
+    borderRadius: 8
+  }}>
+    Entrar
+  </button>
+</a>
